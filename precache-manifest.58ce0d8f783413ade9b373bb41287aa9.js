d09f670f586d20fd539b0940489542a5fea89d7f
salwaelmohandes <60805227+salwaelmohandes@users.noreply.github.com>
2020-04-13 19:52:49 -0400

    Updates

---
+++ precache-manifest.58ce0d8f783413ade9b373bb41287aa9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3edc6e2747d18fa25590e1916ce99237",
+    "url": "/course-directory/index.html"
+  },
+  {
+    "revision": "f8d9b7985b402388e2f3",
+    "url": "/course-directory/static/css/main.3a22b66b.chunk.css"
+  },
+  {
+    "revision": "0b1a7071ba350c50d099",
+    "url": "/course-directory/static/js/2.9be96194.chunk.js"
+  },
+  {
+    "revision": "b73db60337a63448fc0f0ed577e37ab5",
+    "url": "/course-directory/static/js/2.9be96194.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f8d9b7985b402388e2f3",
+    "url": "/course-directory/static/js/main.809d99c0.chunk.js"
+  },
+  {
+    "revision": "de93b7aa1e262e5adaad",
+    "url": "/course-directory/static/js/runtime-main.2d6d2bf2.js"
+  }
+]);
